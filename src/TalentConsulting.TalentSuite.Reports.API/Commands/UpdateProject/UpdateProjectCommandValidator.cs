ee35863c65347dcbf1519875ff3cb627b3434227
Martin-Belton-gov <martin.belton@digital.education.gov.uk>
2023-01-13 15:11:38 +0000

    Hooked up Initial Files

---
+++ src/TalentConsulting.TalentSuite.Reports.API/Commands/UpdateProject/UpdateProjectCommandValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+
+namespace TalentConsulting.TalentSuite.Reports.API.Commands.UpdateProject;
+
+public class UpdateProjectCommandValidator : AbstractValidator<UpdateProjectCommand>
+{
+    public UpdateProjectCommandValidator()
+    {
+        RuleFor(v => v.ProjectDto)
+            .NotNull();
+
+        RuleFor(v => v.ProjectDto.Id)
+            .MinimumLength(1)
+            .NotNull()
+            .NotEmpty();
+
+        RuleFor(v => v.ProjectDto.ClId)
+            .MinimumLength(1)
+            .NotNull()
+            .NotEmpty();
+
+        RuleFor(v => v.ProjectDto.Name)
+            .MinimumLength(1)
+            .NotNull()
+            .NotEmpty();
+
+        RuleFor(v => v.ProjectDto.Reference)
+            .MinimumLength(1)
+            .NotNull()
+            .NotEmpty();
+
+        RuleFor(v => v.ProjectDto.StartDate)
+            .NotNull();
+
+        RuleFor(v => v.ProjectDto.EndDate)
+            .NotNull();
+    }
+}
---
+++ src/TalentConsulting.TalentSuite.Reports.API/Queries/GetProjects/GetProjectsCommand.cs
@@ -0,0 +1,63 @@
+﻿using Ardalis.GuardClauses;
+using Ardalis.Specification;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TalentConsulting.TalentSuite.Reports.Common;
+using TalentConsulting.TalentSuite.Reports.Common.Entities;
+using TalentConsulting.TalentSuite.Reports.Core.Entities;
+using TalentConsulting.TalentSuite.Reports.Infrastructure.Persistence.Repository;
+
+namespace TalentConsulting.TalentSuite.Reports.API.Queries.GetProjects;
+
+public class GetProjectsCommand : IRequest<PaginatedList<ProjectDto>>
+{
+    public GetProjectsCommand(int? pageNumber, int? pageSize)
+    {
+        PageNumber = pageNumber != null ? pageNumber.Value : 1;
+        PageSize = pageSize != null ? pageSize.Value : 1;
+    }
+
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+}
+
+public class GetProjectsCommandHandler : IRequestHandler<GetProjectsCommand, PaginatedList<ProjectDto>>
+{
+    private readonly ApplicationDbContext _context;
+
+    public GetProjectsCommandHandler(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<PaginatedList<ProjectDto>> Handle(GetProjectsCommand request, CancellationToken cancellationToken)
+    {
+        var entities = _context.Projects;
+            
+
+        if (entities == null)
+        {
+            throw new NotFoundException(nameof(Project), "Projects");
+        }
+
+        var filteredReferrals = await entities.Select(x => new ProjectDto(
+            x.Id,
+            x.ClId,
+            x.Name,
+            x.Reference,
+            x.StartDate,
+            x.EndDate
+            )).ToListAsync();
+
+        if (request != null)
+        {
+            var pagelist = filteredReferrals.Skip((request.PageNumber - 1) * request.PageSize).Take(request.PageSize).ToList();
+            var result = new PaginatedList<ProjectDto>(filteredReferrals, pagelist.Count, request.PageNumber, request.PageSize);
+            return result;
+        }
+
+        return new PaginatedList<ProjectDto>(filteredReferrals, filteredReferrals.Count, 1, 10);
+
+
+    }
+}
+
